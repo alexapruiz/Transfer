4f18737036b3d4b4de74c1291b4b96c410508fb8
alexapruiz <48995416+alexapruiz@users.noreply.github.com>
2026-03-12 14:02:51 -0300

    Add files via upload

---
+++ migrar-repo.ps1
@@ -0,0 +1,14 @@
+try {
+    Write-Host "============================"
+    Write-Host "  MIGRADOR DE REPOSITORIOS  "
+    Write-Host "============================"
+    Write-Host ""
+
+}
+catch {
+    Write-Host ""
+}
+finally {
+    Write-Host ""
+
+}
